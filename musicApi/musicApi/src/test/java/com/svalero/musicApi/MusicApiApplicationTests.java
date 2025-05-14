2c5dcda48d972e856d8201a657ff374369c597a0
Mar <mandrescasabon@hotmail.com>
2025-05-14 23:31:27 +0200

    :tada: init commit

---
+++ musicApi/musicApi/src/test/java/com/svalero/musicApi/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.musicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
