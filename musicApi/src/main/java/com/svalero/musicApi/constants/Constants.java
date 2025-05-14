9c3f21b7c025eeafafa24571a91612cfd01ea79d
Mar <mandrescasabon@hotmail.com>
2025-05-15 01:15:44 +0200

    :sparkles: add user, artist & albums models

---
+++ musicApi/src/main/java/com/svalero/musicApi/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.svalero.musicApi.constants;
+
+public class Constants {
+
+    public enum Role{
+        USER, ADMIN
+    }
+}
---
+++ musicApi/src/main/java/com/svalero/musicApi/domain/Album.java
@@ -0,0 +1,50 @@
+package com.svalero.musicApi.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="album")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(length = 100)
+    private String front;
+
+    @Column(length = 70)
+    private String genre;
+
+    @Column
+    @Min(1)
+    private String tracks;
+
+    @Column
+    @Min(0)
+    private String duration;
+
+    @ManyToOne
+    @JoinColumn(name = "artist_id")
+    //Evita serialización infinita de JSON, al estar relacionadas team-player entra en bucle
+    @JsonManagedReference(value = "artist_albums")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Artist artist;
+}
